efc17f14d297c393cce41a4e44035e8f3e7cb798
Rupal Jain <rupal.jain@nagarro.com>
2021-06-02 00:10:29 +0530

    Add project files.

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie                  //POCO represent state and behaviour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    // page to render these details would be under
+    // /movies/random
+}
